47171356ffb890e44f05f792afc1985ca969fcc0
Averu <mikhailov.pavel.28@gmail.com>
2026-06-03 13:23:53 +0500

    base class lol

--- src/controllers/mini_game_controllers/base_mini_game.py
+++ src/controllers/mini_game_controllers/base_mini_game.py
@@ -1,2 +1,31 @@
 from abc import ABC
 from abc import abstractmethod
+
+class BaseMiniGameController(ABC):
+    def __init__(self, grid, view):
+        self.grid = grid
+        self.view = view
+        self.score = 0
+        self.finished = False
+    
+    @abstractmethod
+    def setup(self, data=None):
+        pass
+    
+    @abstractmethod
+    def handle_event(self, event):
+        pass
+    
+    @abstractmethod
+    def update(self, val):
+        pass
+    
+    def get_score(self):
+        return self.score
+    
+    def is_finished(self):
+        return self.finished
+    
+    @abstractmethod
+    def get_hint(self):
+        pass
